02e1dcad37b54e969b6f496139c57bab18174fcb
samuelitwaru <samuelitwaru@gmail.com>
2022-05-10 10:55:40 +0300

    change db to sqlite

--- wex/settings.py
+++ wex/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'wex.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {'ENGINE': 'django.db.backends.mysql','NAME': os.environ["WEX_DB_NAME"],'USER': os.environ["WEX_DB_USER"],'PASSWORD': os.environ["WEX_DB_PASSWORD"]},
-    # 'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}
+    # 'default': {'ENGINE': 'django.db.backends.mysql','NAME': os.environ["WEX_DB_NAME"],'USER': os.environ["WEX_DB_USER"],'PASSWORD': os.environ["WEX_DB_PASSWORD"]},
+    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}
 }
 
 
