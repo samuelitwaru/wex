4d894469b8f43d6612d044cb99f71a84657baf99
samuelitwaru <samuelitwaru@gmail.com>
2022-06-02 12:34:29 +0300

    static files

--- wex/settings.py
+++ wex/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
