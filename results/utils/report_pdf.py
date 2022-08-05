dbd369b02898f67b6e512c7e5588f21fc5c468f6
samuelitwaru <samuelitwaru@gmail.com>
2022-08-05 06:41:41 +0300

    pdf report update

--- results/utils/report_pdf.py
+++ results/utils/report_pdf.py
@@ -74,7 +74,7 @@ title_style = ParagraphStyle(name='title',
                              wordWrap=True,
                              alignment=1)
 
-space = Spacer(0, 0.2 * inch)
+space = Spacer(0, 0.02 * inch)
 hr = HRFlowable(width=560,
                 thickness=2,
                 color=colors.black,
